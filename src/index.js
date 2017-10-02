6ad558b293fd0fed7b1cf62b0d4ee593ce883bef
justinbutt3r <justinbutt3r@yahoo.com>
2017-10-02 16:03:55 +0200

    Revert "Added redux"
    This reverts commit c47603413cc9b5dfe4fd604508a4e4b1c8474046.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import store, { history } from './store';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <div><App /></div>
-        </ConnectedRouter>
-    </Provider>
+    <Router>
+        <App />
+    </Router>
     , document.getElementById('root'));
 registerServiceWorker();
