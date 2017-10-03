2243f8e2d681d98b11aef26767cb4cae886cb3f8
justinbutt3r <justinbutt3r@yahoo.com>
2017-10-03 13:42:22 +0200

    added support for redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 import {
     ApolloProvider,
     ApolloClient,
     createNetworkInterface
 } from 'react-apollo'
+import { ConnectedRouter as Router } from 'react-router-redux'
+import buildStore, { history } from './store'
 import './index.scss';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -20,13 +22,11 @@ const client = new ApolloClient({
     connectToDevTools: process.env.NODE_ENV === 'development'
 })
 
-
-
-
+const store = buildStore(client);
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <Router>
+    <ApolloProvider store={store} client={client}>
+        <Router history={history}>
             <App />
         </Router>
     </ApolloProvider>
